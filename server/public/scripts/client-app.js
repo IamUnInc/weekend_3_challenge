131808ae6f27e280c525e09fd368f634ec827605
IamUnInc <nmatecki@nicholas-mateckis-powerbook-g4.local>
2016-08-15 14:11:36 -0500

    fixed the submit issue

--- server/public/scripts/client-app.js
+++ server/public/scripts/client-app.js
@@ -3,98 +3,53 @@ console.log("im working");
 $('#calc-form').on('click', '#restart', function () {
 location.reload();
 });
+
+$('#calc-form').on('click', '#equals', function () {
+  event.preventDefault();
+  equals();
+});
+
+var numbers = {};
+
 //adding numbers
 $('#calc-form').on('click', '#add', function () {
     event.preventDefault();
     console.log("trying to add");
-  var numbers = {};
+    numbers['type'] = 'add';
 
-  $.each($('#calc-form').serializeArray(), function (i, field) {
-      numbers[field.name] = field.value;
-      numbers['type'] = 'add';
-    });
   console.log('Numbers submitted are ', numbers);
-$.ajax({
-  type: 'POST',
-  url: '/number',
-  data: numbers,
-  success: function (response) {
-    console.log('POST /Calculator works!');
-    getNumbers();
-  },
-
-  error: function (response) {
-    console.log('Attempted POST /songs, did not work');
-    alert("Please Check Your Entry");
-  }
-  });
 });
 //subtracting numbers
 $('#calc-form').on('click', '#subtract', function () {
     event.preventDefault();
     console.log("trying to subtract");
-  var numbers = {};
+    numbers['type'] = 'subtract';
 
-  $.each($('#calc-form').serializeArray(), function (i, field) {
-      numbers[field.name] = field.value;
-      numbers['type'] = 'subtract';
-    });
   console.log('Numbers submitted are ', numbers);
-
-$.ajax({
-  type: 'POST',
-  url: '/number',
-  data: numbers,
-  success: function (response) {
-    console.log('POST /Calculator works!');
-    getNumbers();
-  },
-
-  error: function (response) {
-    console.log('Attempted POST /songs, did not work');
-    alert("Please Check Your Entry");
-  }
   });
-});
 //multipyling numbers
 $('#calc-form').on('click', '#multiply', function () {
     event.preventDefault();
     console.log("trying to multiply");
-  var numbers = {};
+    numbers['type'] = 'multiply';
 
-  $.each($('#calc-form').serializeArray(), function (i, field) {
-      numbers[field.name] = field.value;
-      numbers['type'] = 'multiply';
-    });
   console.log('Numbers submitted are ', numbers);
-
-$.ajax({
-  type: 'POST',
-  url: '/number',
-  data: numbers,
-  success: function (response) {
-    console.log('POST /Calculator works!');
-    getNumbers();
-  },
-
-  error: function (response) {
-    console.log('Attempted POST /songs, did not work');
-    alert("Please Check Your Entry");
-  }
-  });
 });
 //dividing numbers
 $('#calc-form').on('click', '#divide', function () {
     event.preventDefault();
     console.log("trying to divide");
-  var numbers = {};
-
-  $.each($('#calc-form').serializeArray(), function (i, field) {
-      numbers[field.name] = field.value;
       numbers['type'] = 'divide';
-    });
   console.log('Numbers submitted are ', numbers);
 
+});
+
+
+
+function equals() {
+  $.each($('#calc-form').serializeArray(), function (i, field) {
+      numbers[field.name] = field.value;
+    });
 $.ajax({
   type: 'POST',
   url: '/number',
@@ -103,13 +58,12 @@ $.ajax({
     console.log('POST /Calculator works!');
     getNumbers();
   },
-
   error: function (response) {
     console.log('Attempted POST /songs, did not work');
     alert("Please Check Your Entry");
   }
   });
-});
+}
 
 function getNumbers() {
   $.ajax({
